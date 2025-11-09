d9ea676ca10a8fee56c59eea610a9b29e9af919d
manahilabid <soniaaid806@gmail.com>
2025-11-09 04:32:37 -0700

    Math's operators

---
+++ class_03/class_03/Controllers/calculatorController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace class_03.Controllers
+{
+    public class calculatorController : Controller
+    {
+        // GET: calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult index(FormCollection obj)
+        {
+            try
+            {
+
+                int num1 = Convert.ToInt32(obj["num1"]);
+                int num2 = Convert.ToInt32(obj["num2"]);
+
+                string opt = obj["Operator"];
+                string result = string.Empty;
+                switch (opt)
+                {
+                    case "add":
+                        {
+                            result = $"Addition: {num1 + num2}";
+                            break;
+                        }
+                    case "sub":
+                        {
+                            result = $"Subltion: {num1 - num2}";
+                            break;
+                        }
+                    case "multi":
+                        {
+                            result = $"Multiple: {num1 * num2}";
+                            break;
+                        }
+                    case "div":
+                        {
+                            result = $"Division:{num1 / num2}";
+                            break;
+                        }
+                        ViewBag.result = result;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.result = $"Error:{ex.Message}";
+
+            }
+            return View();
+        }
+    }
+
+}
---
+++ class_03/class_03/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="class_03.MvcApplication" Language="C#" %>
