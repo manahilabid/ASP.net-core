9daa04f8bd20146e9df5094694fcefe3cee0fda0
manahilabid <soniaaid806@gmail.com>
2025-11-10 09:59:13 -0700

    view bag , temp data , view data

---
+++ class_04/class_04/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace class_04.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.data = "View Bag Method";
+            ViewBag["msg"] = "View bag object";
+            TempData["message"] = "temp data object";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ class_04/class_04/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="class_04.MvcApplication" Language="C#" %>
